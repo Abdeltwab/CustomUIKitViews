ef4715bc37bce4b696490db2c9768cc743915ad0
Abdeltwab <Abdeltwab.elhussin@gmail.com>
2021-09-30 23:26:47 +0200

    add curved arrow view remaining methods

--- CurvedArrow/CurvedArrow.swift
+++ CurvedArrow/CurvedArrow.swift
@@ -17,12 +17,11 @@ class CurvedArrow: UIView {
     var arrowsCount = 0
     var lineWidth = 0
 
-
     // MARK: - life Cycle
 
     override init(frame: CGRect) {
         super.init(frame: frame)
-        self.setupView()
+        setupView()
     }
 
     required init?(coder: NSCoder) {
@@ -31,32 +30,66 @@ class CurvedArrow: UIView {
 }
 
 extension CurvedArrow {
-    
-    private func setupView(){
-        containerLayer.frame = self.bounds
-        self.addArrowsLayer()
-        self.layer.addSublayer(containerLayer)
-
+    private func setupView() {
+        containerLayer.frame = bounds
+        addArrowsViews()
+        layer.addSublayer(containerLayer)
     }
-    
-    private func addArrowsLayer(){
-        
+
+    private func addArrowsViews() {
+        for i in 0 ..< arrowsCount {
+            addArrowLayer(i: i)
+        }
+    }
+
+    private func addArrowLayer(i: Int) {
+        let layer = getArrowLayer()
+        setArrowStartAngle(for: i)
+        setArrowEndAngle(for: i)
+        layer.path = getArrowPath()
+        containerLayer.addSublayer(layer)
+    }
+
+    private func getArrowLayer() -> CAShapeLayer {
+        let layer = CAShapeLayer()
+        configureLayer(layer)
+        return layer
+    }
+
+    private func getArrowPath() -> CGPath {
+        let diameter = 2 * CGFloat.pi
+        let radius = CGFloat(180)
+        let path = UIBezierPath()
+        path.addArc(withCenter: center,
+                    radius: radius,
+                    startAngle: diameter * startAngle,
+                    endAngle: diameter * endAngle,
+                    clockwise: true)
+        let currentPoint = path.currentPoint
+        let p = CGPoint(x: currentPoint.x, y: currentPoint.y)
+        path.move(to: p)
+        path.close()
+        return path.cgPath
+    }
+
+    private func configureLayer(_ layer: CAShapeLayer) {
+        layer.lineWidth = CGFloat(lineWidth)
+        layer.fillColor = UIColor.clear.cgColor
+        layer.strokeColor = UIColor.red.cgColor
     }
 }
 
+// MARK: - Helpers
 
-
-//MARK: - Helpers
 extension CurvedArrow {
-    
-    private func setArrowStartAngle(for arrowNumber :Int){
-        self.startAngle = arrowNumber == 0 ? CGFloat(arrowsCount / arrowsCount) : CGFloat(arrowNumber) / CGFloat(arrowsCount)
+    private func setArrowStartAngle(for arrowNumber: Int) {
+        startAngle = arrowNumber == 0 ? CGFloat(arrowsCount / arrowsCount) : CGFloat(arrowNumber) / CGFloat(arrowsCount)
     }
-    
-    private func setArrowEndAngle(for arrowNumber:Int) {
-        self.endAngle = CGFloat(Double(arrowNumber) + 0.95) / CGFloat(arrowsCount)
+
+    private func setArrowEndAngle(for arrowNumber: Int) {
+        endAngle = CGFloat(Double(arrowNumber) + 0.95) / CGFloat(arrowsCount)
     }
-    
+
     private func getArrowCenter() -> CGPoint {
         let center = CGPoint(x: containerLayer.bounds.width / 2,
                              y: containerLayer.bounds.height / 2)
