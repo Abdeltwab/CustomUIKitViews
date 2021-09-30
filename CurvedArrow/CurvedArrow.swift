daff30eb1a958e416ad26a85bab5f4775715c56c
Abdeltwab <Abdeltwab.elhussin@gmail.com>
2021-09-30 23:03:22 +0200

    add required variables for the view

--- CurvedArrow/CurvedArrow.swift
+++ CurvedArrow/CurvedArrow.swift
@@ -8,7 +8,26 @@
 import UIKit
 
 class CurvedArrow: UIView {
+    // MARK: - Variables
+
+    private let mainLayer = CAShapeLayer()
+    private var startAngle = CGFloat()
+    private var endAngle = CGFloat()
+    private var arrowDirection = false
+    var arrowsCounter = 0
+    var lineWidth = 0
 
 
+    // MARK: - life Cycle
 
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+extension CurvedArrow {
 }
