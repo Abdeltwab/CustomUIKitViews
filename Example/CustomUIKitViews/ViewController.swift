f6d69a3174a26bd961a4cdd622be875d914c8a94
Abdeltwab <Abdeltwab.elhussin@gmail.com>
2021-10-01 02:11:42 +0200

    add CheckMark View

--- Example/CustomUIKitViews/ViewController.swift
+++ Example/CustomUIKitViews/ViewController.swift
@@ -15,12 +15,13 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
         let rect = CGRect(x: 50, y: 50, width: 0, height: 0)
-        let radius = 50
-        let loader = CurvedArrow(frame: rect, arrowsCount: 2, lineWidth: 5, radius: radius)
-        view.addSubview(loader)
-
-        let cv = Circle(frame: rect, radius: radius, color: .green)
-        view.addSubview(cv)
+        let chkMArk = CheckMark(frame: rect,color: .red)
+        view.addSubview(chkMArk)
+//        let radius = 50
+//        let loader = CurvedArrow(frame: rect, arrowsCount: 2, lineWidth: 5, radius: radius)
+//        view.addSubview(loader)
+//        let cv = Circle(frame: rect, radius: radius, color: .green)
+//        view.addSubview(cv)
 
 
     }
--- Views/CheckMark/CheckMark.swift
+++ Views/CheckMark/CheckMark.swift
@@ -7,8 +7,51 @@
 
 import UIKit
 
-class CheckMark: UIView {
+public class CheckMark: UIView {
 
+    private let containerLayer = CAShapeLayer()
+    public var radius = 50
+    public var color = UIColor.white
+
+    // MARK: - life Cycle
     
+    public override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.setupView()
+    }
+    
+    public  init(frame: CGRect , color:UIColor) {
+        super.init(frame: frame)
+        self.color = color
+        self.setupView()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 
 }
+
+extension CheckMark{
+    
+    private func setupView() {
+        containerLayer.frame = bounds
+        self.addCheckMarkLayer()
+        layer.addSublayer(containerLayer)
+    }
+
+    private func addCheckMarkLayer() {
+        let checkMarkLayer = CAShapeLayer()
+        let path = UIBezierPath()
+        path.move(to: CGPoint(x: 20 , y: 50))
+        path.addLine(to: CGPoint(x: 45, y:  75))
+        path.addLine(to: CGPoint(x: 70, y: 20))
+        checkMarkLayer.fillColor = UIColor.clear.cgColor
+        checkMarkLayer.strokeColor = color.cgColor
+        checkMarkLayer.lineWidth = 5
+        checkMarkLayer.path = path.cgPath
+        containerLayer.addSublayer(checkMarkLayer)
+    }
+}
+
+
